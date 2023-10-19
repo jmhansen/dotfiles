d43515837f86a9b8cd137a952360f849cfacdea5
Jon Hansen <jon@simplyagree.com>
2023-10-19 10:13:12 -0500

    Adds git completion and nvm

--- zshrc.sh
+++ zshrc.sh
@@ -11,3 +11,11 @@ eval "$(pyenv init -)"
 
 # pyenv-virtualenv
 eval "$(pyenv virtualenv-init -)"
+
+# git completion
+autoload -Uz compinit && compinit
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
